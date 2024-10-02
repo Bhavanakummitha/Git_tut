e19612fe6148f2b738ddae903170a1f40a8bee87
Bhavanakummitha <bhavanareddykummitha@gmail.com>
2024-10-02 11:20:09 +0530

    commiting Range and list

---
+++ Range.py
@@ -0,0 +1,16 @@
+st="Hello"
+for i in range(len(st)):
+    print(i)
+
+
+st='core#567'
+for i in range(len[st]):
+    print(i,st[i])
+
+    st='python@456'
+    if ch>='A' and ch<='Z' or ch>='a' and ch<='z':
+     for i in st:
+        print(i)
+        i+=1
+        
+        
