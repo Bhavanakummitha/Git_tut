e8f6688dd9a5b0fef23d85a12b72e16b024e5c68
Bhavanakummitha <bhavanareddykummitha@gmail.com>
2024-10-03 12:16:02 +0530

    commiting specialchar to master

---
+++ specialchar.py
@@ -0,0 +1,15 @@
+st='Bhavana@310'
+for i in st:
+    if i>='!' and i<='@':
+        print(i)
+
+        st="Bhavana@310"
+        for i in range(len(st)):
+            if st[i]>='!' and st[i]<='&':
+                print(st[i])
+
+                st="Bhavana@310"
+                i=0
+                if st[i]>='!' and st[i]<='&':
+                  print(i)
+                i+=1
